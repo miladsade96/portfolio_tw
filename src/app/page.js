f062c7b03ad2e720eaded58773f007fd60a09446
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-02-27 14:43:29 +0330

    Finishing section 2

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,10 @@ import {
 } from "react-icons/ai";
 import Image from "next/image";
 import profileImage from "../../public/profile-removedBackground.png";
+import design from "../../public/design.jpg";
+import code from "../../public/code.jpg";
+import consulting from "../../public/consulting.jpg";
+
 export default function Home() {
   return (
     <div>
@@ -43,13 +47,86 @@ export default function Home() {
             <AiFillLinkedin />
             <AiFillTwitterSquare />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <div className="relative mx-auto bg-gradient-to-b from-teal-600 to-teal-100 rounded-full w-80 h-80 mt-20 overflow-hidden mb-10">
             <Image
               src={profileImage}
               alt="Milad Sadeghi Profile Image"
               layout="fill"
               objectFit="cover"
             />
+          </div>
+        </section>
+        <section>
+          <div className="mb-12 text-center">
+            <h3 className="text-3xl py-1">About me</h3>
+            <p className="text-md py2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Adipisci, animi beatae cumque, esse
+              <span className="text-teal-500"> milad</span> ex minus
+              necessitatibus nisi non odio omnis optio quasi totam ut vitae.
+              Alias cum inventore quia!
+            </p>
+          </div>
+          <div className="mb-12 text-center">
+            <h3 className="text-3xl py-1">Services i offer</h3>
+            <p className="text-md py2 leading-8 text-gray-800">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Adipisci, animi beatae cumque, esse
+              <span className="text-teal-500"> milad</span> ex minus
+              necessitatibus nisi non odio omnis optio quasi totam ut vitae.
+              Alias cum inventore quia!
+            </p>
+          </div>
+          <div>
+            <div className="flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                className="rounded-2xl"
+                src={design}
+                alt="design image"
+                width={100}
+                height={100}
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools i use</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                className="rounded-2xl"
+                src={code}
+                alt="design image"
+                width={100}
+                height={100}
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">Smart coding</h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools i use</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="flex flex-col items-center text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                className="rounded-2xl"
+                src={consulting}
+                alt="design image"
+                width={100}
+                height={100}
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Detailed consulting
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools i use</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
           </div>
         </section>
       </main>
